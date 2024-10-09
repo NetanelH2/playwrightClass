7068f834933617149ae2879d1a2181a45c234532
Netanel Harush <netanelharush2@gmail.com>
2024-10-09 21:28:10 +0300

    add Google Search Exercise

---
+++ tests/searchGoogle.spec.js
@@ -0,0 +1,15 @@
+import {expect, test} from '@playwright/test'
+
+test.describe.only('Google Search Bar', () => {
+    test('Search for playwright on Google website', async ({page}) => {
+        await page.goto(
+            'https://www.google.com/webhp?hl=iw&sa=X&ved=0ahUKEwjejK3p4YGJAxW9RKQEHauVD-gQPAgI',
+        )
+        let searchBarLocator = page.locator('.gLFyf')
+        await searchBarLocator.fill('Playwright Docs')
+        let searchButton = page.getByLabel('חיפוש ב-Google').first()
+        await searchButton.click()
+        let searchResultPage = page.locator('#rcnt')
+        await expect(searchResultPage).toContainText('https://playwright.dev')
+    })
+})
